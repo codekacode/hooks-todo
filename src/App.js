291fece486488473851321a0a845fc92b26ca504
Codekacode <karem.quiroz.m@gmail.com>
2022-04-13 21:52:52 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import {useReducer } from 'react';
+import { Button } from 'react-bootstrap';
+
+
+const initialState = {
+  count: 0
+}
+
+function reducer(state, action){
+  switch(action.type){
+    case "increment":
+      return {count: state.count + 1}
+    case "decrement":
+      return {count: state.count - 1}
+    case "reset":
+      return initialState
+    default:
+      return initialState
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  // const value = useContext(UserContext)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Count: {state.count}
+      <br />
+      <Button onClick={() => dispatch({type:'increment'})}>Increment</Button>
+      <Button onClick={() => dispatch({type:'decrement'})}>Decrement</Button>
+      <Button onClick={() => dispatch({type:'reset'})}>Reset</Button>
     </div>
   );
 }
